50afe43ca8f1ac6203d33026feab9156000186ad
PucLucasCampos <lucas.cr16@puccampinas.edu.br>
2024-11-05 20:19:17 -0300

    Aula Estrutura de Repetição

---
+++ src/EstruturasDeRepeticao/AulaEstruturasDeRepeticao.java
@@ -0,0 +1,41 @@
+package EstruturasDeRepeticao;
+
+public class AulaEstruturasDeRepeticao {
+    public static void main(String[] args) {
+        // while, do while, for
+//        int count = 0;
+
+//        while(count < 10){
+//            System.out.println(++count); // 10
+//            //count++ // 9
+//        }
+
+
+//        do {
+//            System.out.println(++count);
+//        }while (count < 10);
+
+
+//        for(int i = 0; i < 10; i++){
+//            System.out.println(i);
+//        }
+
+        // -----------------------------------------------------
+        // Imprima todos os numeros pares de 0 até 1000000
+//        for(int i = 0; i < 1000000; i++){
+//            if(i % 2 == 0) System.out.println(i);
+//        }
+
+        // -----------------------------------------------------
+        // Imprima os primeiros 25 numeros de um dado valor. Por exemplo, 50
+//        int valorMax = 50;
+//        for (int i = 0; i <= valorMax; i++) {
+//            if(i > 25) break;
+//            System.out.println(i);
+//        }
+
+        // -----------------------------------------------------
+
+
+    }
+}
---
+++ src/EstruturasDeRepeticao/ExercicioAulaEstruturaDeRepeticaoBreak.java
@@ -0,0 +1,15 @@
+package EstruturasDeRepeticao;
+
+public class ExercicioAulaEstruturaDeRepeticaoBreak {
+    public static void main(String[] args) {
+        // Dado o valor de um carro,  descubra em quantas vezes ele pode ser parcelado
+        // Condição: valorParcela >= 1000
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela < valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+
+            if(valorParcela < 1000) break;
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
