51c0571e5cf7aa63fa3fcaed014afa2072afe533
PucLucasCampos <lucas.cr16@puccampinas.edu.br>
2024-11-06 19:44:00 -0300

    Aula Introdução a Classes

---
+++ src/OrientacaoObjetos/Classes/AulaClasses.java
@@ -0,0 +1,22 @@
+package OrientacaoObjetos.Classes;
+
+public class AulaClasses {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        Estudante estudante2 = new Estudante();
+
+        estudante.nome = "Lucas";
+        estudante.idade = 19;
+        estudante.sexo = 'M';
+
+        estudante2.nome = "Iris";
+        estudante2.idade = 19;
+        estudante2.sexo = 'F';
+
+        System.out.println(estudante.nome); // Lucas
+        System.out.println(estudante.idade); // 19
+        System.out.println(estudante.sexo); // M
+        System.out.println(estudante); // endereço de memoria
+
+    }
+}
---
+++ src/OrientacaoObjetos/Classes/Estudante.java
@@ -0,0 +1,7 @@
+package OrientacaoObjetos.Classes;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
